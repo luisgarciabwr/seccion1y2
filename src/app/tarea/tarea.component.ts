16582b2474425a1aeddbb17cb46e46206f0129e8
Luis Garcia <luis.garcia@bwr.mx>
2020-03-11 14:26:12 -0600

    seccion 1 y 2 terminada

---
+++ src/app/tarea/tarea.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea',
+  templateUrl: './tarea.component.html',
+  styleUrls: ['./tarea.component.css']
+})
+export class TareaComponent implements OnInit {
+  showSecret = false;
+  log = [];
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+  ngOnInit(): void {
+  }
+
+}
